53ac2005756d613a0046fa01738cbacb4ef516e0
James Gough <jpgough@gmail.com>
2023-06-06 20:42:00 +0100

    Update social card naming and links for edits

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/${organizationName}/${projectName}/tree/main/',
+            'https://github.com/village-hall/village-hall.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/${organizationName}/${projectName}/tree/main/',
+            'https://github.com/village-hall/village-hall.github.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
