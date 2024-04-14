e1315143946d50051e279024838fd949ee5da7e0
James Gough <jpgough@gmail.com>
2024-04-14 13:17:55 +0100

    Make instructions for our external calendar clearer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,10 +35,17 @@ export default function Home(): JSX.Element {
       <main>
         <div style={introTextStyle}>
           <p>
-            Our village hall is set in lovely Leicestershire countryside and is located in the heart of the village with the church, local pub and village shop only a few minutes walk away.<br/><br/>
-            Tilton on the Hill village hall is host to weekly events such as “Singing for Fun” and daily events like “Tiddlywinks pre-school”. Whether you are a villager or event organiser, you are assured of a hearty welcome.<br/><br/>
+            Our village hall is set in lovely Leicestershire countryside and is located in the heart of the village with the church, local pub and village shop only a few minutes walk away.<br/>
             The hall is available for hire to all and we can cater for events from birthday parties to weddings and everything in between. Please use the booking form below to start planning your event.
           </p>
+          <p><b>
+            Notes about booking:
+            <ul>
+              <li>Click + on the date you are looking to book, please check if other events are scheduled during your required time</li>
+              <li>We do no accept bookings within 5 days, we do not have enough volunteers to take on last minute bookings</li>
+              <li>The hall can only be booked 6 months in advance, this is to ensure that our repeat bookings are added to the system</li>
+            </ul>
+          </b></p>
         </div>
         <div className="hallmaster">
           <iframe src="https://v2.hallmaster.co.uk/Scheduler/View/11767?startRoom=19251&roomSelect=false" style={hallmasterStyle}></iframe>
